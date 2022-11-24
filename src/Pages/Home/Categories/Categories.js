8b54cf5c40b3772ac4001a8888531324361a9231
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-11-25 01:52:02 +0600

    category created

---
+++ src/Pages/Home/Categories/Categories.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Categories = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+    return (
+        <div className="bg-accent p-10">
+            <div className='text-center text-white'>
+                <h1 className="text-4xl font-bold">Categories</h1>
+                <h3 className="text-2xl font-semibold">All the categories we cam find here</h3>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10 '>
+                {
+                    categories.map(category =>
+                        <Link to='/login'>
+                            <div className="card w-96 image-full ">
+                                <figure><img src={category.img} alt={category.name} /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title text-2xl font-bold">{category.name}</h2>
+                                    <p>{category.types}. you can find here. This is trustworthy and affordable.We will always help you. Want to see the products? Just click anywhere in this box. </p>
+                                </div>
+                            </div>
+                        </Link>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Categories;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Banner from '../../Shared/Banner/Banner';
 import ContactUs from '../../Shared/ContactUs/ContactUs';
+import Categories from '../Categories/Categories';
 
 const Home = () => {
     return (
@@ -10,7 +11,9 @@ const Home = () => {
                 <h1 className="mb-5 text-5xl font-bold text-white uppercase">Welcome to ReycleBIN</h1>
                 <p className="mb-5 text-white">You can buy or sell used products with affordable price. Start working with us. Just click on the <Link to='/login' className="text-accent">login</Link> button. </p>
             </Banner>
-            <div>Categories</div>
+            <div>
+                <Categories></Categories>
+            </div>
             <div>Advertise</div>
             <ContactUs></ContactUs>
         </div>
