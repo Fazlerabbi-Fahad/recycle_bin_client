f733b726a6bdd3278705db59528f2c3d5bb84710
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-11-24 23:34:45 +0600

    added firebase

---
+++ src/Context/AuthProvider.js
@@ -0,0 +1,60 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import app from '../Firebase/firebase.config';
+
+const AuthContext = createContext();
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    const [loading, setLoading] = useState(true);
+
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const logIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const updateUser = profile => {
+        return updateProfile(auth.currentUser, profile);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+        })
+        return () => unsubscribe;
+    }, [])
+
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+
+    const authInfo = {
+        user,
+        setLoading,
+        createUser,
+        updateUser,
+        logIn,
+        logOut,
+        signInWithGoogle
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -5,7 +5,7 @@ import Logo from "../../../Asssests/Images/Logo.png";
 const Footer = () => {
     return (
         <footer className="footer items-center p-4 bg-neutral text-neutral-content">
-            <div className="items-center grid-flow-col">
+            <div className="items-center lg:grid-flow-col">
                 <Link className="btn btn-ghost normal-case text-2xl font-bold"><img className='w-10' src={`${Logo}`} alt='' />Recycle<span className='text-accent'>BIN</span></Link>
                 <p>Copyright © 2022 - All right reserved</p>
             </div>
